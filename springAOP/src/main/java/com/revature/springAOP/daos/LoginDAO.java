a7d14a20decdea29749bd297c471a6d0dade0359
JesterCharles <charles.jester@revature.com>
2022-02-18 16:12:22 -0500

    Generated skeleton for the LoginDAO from UserService

---
+++ springAOP/src/main/java/com/revature/springAOP/daos/LoginDAO.java
@@ -0,0 +1,28 @@
+package com.revature.springAOP.daos;
+
+import com.revature.springAOP.models.User;
+
+public class LoginDAO {
+	
+	public boolean create(User newUser) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Object findByUsername(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object findByEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public User findByUsernameAndPassword(String username, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
